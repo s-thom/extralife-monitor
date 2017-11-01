eac498881f2dc3ea9767381e5a045f7875559df4
Stuart Thomson <stu.thom.96@gmail.com>
2017-11-02 09:39:20 +1300

    Add Donation and Participant types

---
+++ src/types/Donation.ts
@@ -0,0 +1,26 @@
+import Participant from './Participant';
+
+interface DonationData {
+  message: string;
+  donorName: string;
+  timestamp: number;
+  donationAmount: 15;
+}
+
+export default class Donation {
+  public readonly message: string;
+  public readonly amount: number;
+  public readonly timestamp: Date;
+  public readonly name: string;
+
+  public readonly participant: Participant;
+
+  constructor(data: DonationData, participant: Participant) {
+    this.message = data.message;
+    this.amount = data.donationAmount;
+    this.timestamp = new Date(data.timestamp);
+    this.name = data.donorName;
+
+    this.participant = participant;
+  }
+}
---
+++ src/types/Participant.ts
@@ -0,0 +1,45 @@
+interface ParticipantData {
+  displayName: string;
+  totalRaisedAmount: number;
+  fundraisingGoal: number;
+  participantID: number;
+  avatarImageURL: string;
+}
+
+export default class Participant {
+  // Properties are r/w privately, but readonly publicly
+  // Allows Participant to be updated internally
+  private _name: string;
+  private _goal: number;
+  private _raised: number;
+  private _id: number;
+  private _avatar: string;
+
+  constructor(data: ParticipantData) {
+    this._name = data.displayName;
+    this._goal = data.fundraisingGoal;
+    this._raised = data.totalRaisedAmount;
+    this._id = data.participantID;
+    this._avatar = data.avatarImageURL;
+  }
+  
+  public get name() {
+    return this._name;
+  }
+
+  public get goal() {
+    return this._goal;
+  }
+
+  public get raised() {
+    return this._raised;
+  }
+
+  public get id() {
+    return this._id;
+  }
+
+  public get avatar() {
+    return this._avatar;
+  }
+}
