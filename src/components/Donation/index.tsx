7b631f10a68784a10c003affca69d6fd5e501d42
Stuart Thomson <stu.thom.96@gmail.com>
2017-11-02 20:02:15 +1300

    Donation and Participant components have a remove button

--- src/components/Donation/index.tsx
+++ src/components/Donation/index.tsx
@@ -3,19 +3,32 @@ import * as React from 'react';
 import DonationType from '../../types/Donation';
 import './index.css';
 
-export default function Donation(props: {
-  info: DonationType,
-}) {
+interface Props {
+  info: DonationType;
+  onRemove?: (donation: DonationType) => void;
+}
+
+export default function Donation({
+  info,
+  onRemove,
+}: Props) {
   return (
     <div className="Donation">
       <header className="Donation-header">
-        <span className="Donation-amount">${props.info.amount.toFixed(2)}</span>
-        {/* TODO: Dismissable button */}
+        <span className="Donation-amount">${info.amount.toFixed(2)}</span>
+        {
+          onRemove && (
+            <button
+              className="Donation-remove"
+              onClick={() => onRemove(info)}
+            >❌</button>
+          )
+        }
       </header>
       <div className="Donation-body">
-        <h3 className="Donation-participant">{props.info.participant.name}</h3>
-        <h2 className="Donation-name">{props.info.name}</h2>
-        {props.info.message ? <p>{props.info.message}</p> : <p><em>No message</em></p>}
+        <h3 className="Donation-participant">{info.participant.name}</h3>
+        <h2 className="Donation-name">{info.name}</h2>
+        {info.message ? <p>{info.message}</p> : <p><em>No message</em></p>}
       </div>
     </div>
   );
--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -3,20 +3,34 @@ import * as React from 'react';
 import ParticipantType from '../../types/Participant';
 import './index.css';
 
-export default function Participant(props: {
-  info: ParticipantType,
-}) {
+interface Props {
+  info: ParticipantType;
+  onRemove?: (participant: ParticipantType) => void;
+}
+
+export default function Participant({
+  info,
+  onRemove,
+}: Props) {
   return (
     <div className="Participant">
       <header className="Participant-header">
-        <h2 className="Participant-name">{props.info.name}</h2>
-        <h3 className="Participant-id">{props.info.id}</h3>
+        <h2 className="Participant-name">{info.name}</h2>
+        <h3 className="Participant-id">{info.id}</h3>
+        {
+          onRemove && (
+            <button
+              className="Participant-remove"
+              onClick={() => onRemove(info)}
+            >❌</button>
+          )
+        }
       </header>
       <div>
         <span className="Participant-goal">
-          <span className="Participant-raised">${props.info.raised.toFixed(2)}</span>
+          <span className="Participant-raised">${info.raised.toFixed(2)}</span>
           /
-          <span className="Participant-full-goal">${props.info.goal.toFixed(2)}</span>
+          <span className="Participant-full-goal">${info.goal.toFixed(2)}</span>
         </span>
       </div>
     </div>
